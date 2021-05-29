fa32117a94953f2172756130397a6c823596901f
crosgar <cecilia.rossi.garcia@gmail.com>
2021-05-28 22:42:30 -0700

    set up project folders, files, apiRoutes

---
+++ routes/apiRoutes.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+const router = require('express').Router();
+const db = require('../db/db.json');
+
+router.get('/notes', (req, res) => {
+   res.json(db);
+});
+
+router.post('/notes', ({body}, res) => {
+   const {title, text} = body;
+   const id = Math.floor(Math.random() * 100000);
+   const newNote = {title, text, id};
+
+   db.push(newNote);
+
+   fs.writeFileSync('db/db.json', JSON.stringify(db));
+
+   res.json(db);
+
+});
+
+router.delete('/notes/:id', (req, res) => {
+   const id = req.params.id;
+   const newDb = db.filter(id => {});
+
+   // fs.writeFileSync('db/db.json', JSON.stringify(db));
+
+   // res.json(newDb);
+   
+});
+
+module.exports = router;
